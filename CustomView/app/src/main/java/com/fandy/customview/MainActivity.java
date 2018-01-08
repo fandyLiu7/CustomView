1e0957005d598f8603025967e662b61d12f5cdff
muhongying <609818552@qq.com>
2018-01-08 20:11:49 +0800

    first

---
+++ CustomView/app/src/main/java/com/fandy/customview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.fandy.customview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 自定义view的学习
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取view宽高的几种方式
+    }
+}
